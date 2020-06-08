ddf8afd3e3b146dbe239c354f4860817e9787aa7
Eduardo Marques <emarques07@gmail.com>
2020-06-09 00:29:19 +0100

    .

--- RockPaperScissors/src/Game.java
+++ RockPaperScissors/src/Game.java
@@ -14,6 +14,15 @@ public class Game {
                 currentRound++;
                 continue;
             }
+            else if (p1guess.getWinsTo() == p2guess.ordinal()){
+                player1.addPoint();
+            }
+            else if (p1guess.getLosesTo() == p2guess.ordinal()){
+                player2.addPoint();
+            }
+            currentRound++;
+
+            /*
             else if (p1guess == RpsType.ROCK){
                 if (p2guess == RpsType.PAPER){
                     player2.addPoint();
@@ -43,17 +52,21 @@ public class Game {
                 }
                 currentRound++;
                 continue;
-            }
+            }*/
         }
         if (player1.getPoints() == player2.getPoints()){
             System.out.println("It's a Draw......"+player1.getPoints()+" : "+player2.getPoints());
         }
+
         else if (player1.getPoints() > player2.getPoints()){
             System.out.println(player1.getName()+ " Wins! "+player1.getPoints()+" : "+player2.getPoints());
         }
-       else if (player1.getPoints() < player2.getPoints()){
+        else if (player1.getPoints() < player2.getPoints()){
            System.out.println(player2.getName() + " Wins! "+player1.getPoints()+" : "+player2.getPoints());
         }
+
+
+
 
     }
 
--- RockPaperScissors/src/RpsType.java
+++ RockPaperScissors/src/RpsType.java
@@ -1,5 +1,22 @@
 public enum  RpsType {
-    ROCK,
-    PAPER,
-    SCISSORS
+    ROCK(2,1),
+    PAPER(0,2),
+    SCISSORS(1,0);
+
+    private int winsTo;
+    private int losesTo;
+
+    private RpsType(int winsTo, int losesTo) {
+        this.winsTo=winsTo;
+        this.losesTo=losesTo;
+    }
+    public int getWinsTo(){
+        return winsTo;
+    }
+    public int getLosesTo(){
+        return losesTo;
+    }
+
+
+
 }
